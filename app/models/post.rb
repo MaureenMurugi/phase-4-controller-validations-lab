5724c947ee153e10b61da46cc3829735f7c82f06
Maureen Murugi <maureen.murugi@student.moringaschool.com>
2022-11-28 06:20:57 +0300

    Completed assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 100}
+    validates :category, presence: ["fiction", "non-fiction"] true
 end
